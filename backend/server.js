0a62f7a80c1a0c620816a1bbb3344e9b8d793ae2
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-07-06 10:38:00 +0500

    Add CORS configuration to backend server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const connectDb = require("./config/dbConnect"); // Importing the dbConnect module
+const cors = require("cors");
 
 require("dotenv").config();
 
 const app = express();
 const port = 3000;
+const origin = process.env.CORS_ORIGIN || "http://localhost:5173";
+
 app.use(express.json());
+app.use(
+  cors({
+    origin,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello, World!....");
